32caca904176059f7098fa5e9ba517afb112af6c
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-07-20 12:50:29 +0430

    make Articles model and show that in admin panel

--- articles/admin.py
+++ articles/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Articles
 # Register your models here.
+
+@admin.register(Articles)
+class ArticlesAdmin(admin.ModelAdmin):
+    pass
--- articles/models.py
+++ articles/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Articles(models.Model):
+    STATUS_CHOICES = [
+        ('p', 'Publish'),
+        ('d', 'Draft')
+    ]
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(max_length=100, unique=True)
+    description = models.TextField()
+    publish = models.DateTimeField(default=timezone.now)
+    create = models.DateTimeField(auto_now_add=True)
+    update = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES)
