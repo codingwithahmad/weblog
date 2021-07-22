0dc4a26a600b30af7ce4abf679499a75ca6460eb
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-07-22 13:09:54 +0430

    add author to Articles model

--- articles/models.py
+++ articles/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Articles(models.Model):
@@ -10,6 +11,7 @@ class Articles(models.Model):
 
     title = models.CharField(max_length=200)
     slug = models.SlugField(max_length=100, unique=True)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     description = models.TextField()
     publish = models.DateTimeField(default=timezone.now)
     create = models.DateTimeField(auto_now_add=True)
