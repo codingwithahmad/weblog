487220885d54b7a51b26924065f42cc9fcfd7d55
codingwithahmad <amirahmad.seraji1996@gmail.com>
2021-07-29 17:07:40 +0430

    fix scurity issue

--- myweblog/settings.py
+++ myweblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-msteydl2ulktis8#!&j2rf@$odxe3h0osbi!hb-amc6hy84)&0'
+SECRET_KEY = config.API_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
